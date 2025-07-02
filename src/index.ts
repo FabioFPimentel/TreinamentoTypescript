cddc40c1ece6bf697d7fe07ab047d4f1eafaa864
Fábio Ferreira Pimentel <fabio.pimentel@zema.com>
2025-07-02 14:03:19 -0300

    Inicio da aula 2

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ function add(num1: number, num2: number) {
 
 let result = add(5, 8)
 console.log(result)
+
+// aula 20 / any
+let price = 40
+let item = 'maca'
+let inStore = true
+
+let value
+value = 10
+value = '10'
